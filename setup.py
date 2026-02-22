04fdcbf6ba8225eca43962733b9b4990a7d34e38
AmineBarguellil7 <aminebarguellil7@gmail.com>
2026-02-22 16:49:39 +0100

    data_ingestion_preprocessing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube_sentiment_analysis',
+    version= '0.1',
+    author= 'Amine',
+    author_email= 'aminebarguellil7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
